5e720a38a5224269472d17efcb13dc4f3813f40a
allane.cruz <allane.cruz@dcx.ufpb.br>
2026-09-01 14:08:06 -0300

    adicionando - aula 02 - versão 2

--- src/main/java/Aula02.java
+++ src/main/java/Aula02.java
@@ -4,7 +4,14 @@ public class Aula02 {
     public static void main (String[] args){
 
         String nome = JOptionPane.showInputDialog("Digite seu nome: ");
-
         System.out.println("Oi, "+ nome);
+
+        String idadeString = JOptionPane.showInputDialog("Digite sua idade: ");
+        int idade = Integer.parseInt(idadeString);
+        String alturaStr = JOptionPane.showInputDialog("Digite sua altura: ");
+        double altura = Double.parseDouble(alturaStr);
+
+        System.out.println("Suas informações são, idade = "+ idade +" e altura = "+ altura);
+
     }
 }
