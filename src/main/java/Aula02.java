31885b2f6de2de4a5a7609d3fe73093e886c608d
allane.cruz <allane.cruz@dcx.ufpb.br>
2026-09-01 13:53:41 -0300

    adicionando - aula 02

---
+++ src/main/java/Aula02.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Aula02 {
+    public static void main (String[] args){
+
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+
+        System.out.println("Oi, "+ nome);
+    }
+}
